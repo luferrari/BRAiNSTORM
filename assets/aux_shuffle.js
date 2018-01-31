8c2c915f71bac72b249bde7956ea0d24f49dc771
luferrari <lucialuferrari@gmail.com>
2018-01-31 15:42:05 +0100

    thanks github

--- assets/aux_shuffle.js
+++ assets/aux_shuffle.js
@@ -1,11 +1,11 @@
 function counter() {
   if (booted) {
-    if (step < 10) {
+    if (state < 10) {
       background(209, 210, 211);
-      step++;
-      img(b0);
-      roll();
-      design();
+      state++;
+      img(brain0);
+      shuffle();
+      graphics();
     } else {
       noLoop();
     }
@@ -14,64 +14,67 @@ function counter() {
 
 
 
-function roll() {
-  var k1 = 255;
-  var k2 = 255;
-  var k3 = 255;
-  var k4 = 255;
+function shuffle() {
+  var opac1 = 255;
+  var opac2 = 255;
+  var opac3 = 255;
+  var opac4 = 255;
 
-  h = floor(random() * heights.length);
+  hidden = floor(random() * heights.length);
 
-  if (step % 2 !== 0) {
-    switch (h) {
+  if (state % 2 !== 0) {
+    switch (hidden) {
       case 0:
-        k1 = 0;
+        opac1 = 0;
         break;
       case 1:
-        k2 = 0;
+        opac2 = 0;
         break;
       case 2:
-        k3 = 0;
+        opac3 = 0;
         break;
       case 3:
-        k4 = 0;
+        opac4 = 0;
+        break;
+      default:
+        return false;
         break;
     }
   }
 
   textSize(20);
   textAlign(LEFT);
-  textFont(md);
+  textFont(medium);
   fill(254, 250, 224);
 
   f = floor(random() * frontal.length);
   text(frontal[f], 176, heights[0]);
   text(actionF[f], 176, heights[0] + 224);
-  tint(255, k1);
-  img(b1);
+  tint(255, opac1);
+  img(brain1);
 
   p = floor(random() * parietal.length);
   text(parietal[p], 176, heights[1]);
   text(actionP[p], 176, heights[1] + 224);
-  tint(255, k2);
-  img(b2);
+  tint(255, opac2);
+  img(brain2);
 
   o = floor(random() * actionO.length);
   text(occipital[0], 176, heights[2]);
   text(actionO[o], 176, heights[2] + 224);
-  tint(255, k3);
-  img(b3);
+  tint(255, opac3);
+  img(brain3);
 
   t = floor(random() * temporal.length);
   text(temporal[t], 176, heights[3]);
   text(actionT[t], 176, heights[3] + 224);
-  tint(255, k4);
-  img(b4);
+  tint(255, opac4);
+  img(brain4);
 
-  if (step % 2 !== 0) {
+  if (state % 2 !== 0) {
     fill(209, 210, 211);
-    rect(171, heights[h] - 18, 250, 40);
-    rect(171, heights[h] + 200, 400, 35);
+    rect(171, heights[hidden] - 18, 250, 40);
+    rect(171, heights[hidden] + 200, 400, 35);
   }
 
   tint(255, 255);
